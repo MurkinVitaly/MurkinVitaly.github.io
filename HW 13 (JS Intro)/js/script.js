e12100135586100873d6003372415cac0c4e9f11
MurkinVitaly <murkin.vitaly@gmail.com>
2020-10-04 18:04:38 +0300

    Add HW 13 and 14

---
+++ HW 13 (JS Intro)/js/script.js
@@ -0,0 +1,25 @@
+let a = 6
+let b = 3
+console.log (a>b);
+a = 3
+b = 3
+console.log (a==b);
+a = 3
+b = 6
+console.log (a!=b);
+a = 1
+b = ""
+console.log (a===b);
+a="B";
+b="A";
+console.log(a>b);
+a="a";
+b="Z";
+console.log(a>b);
+a=false;
+b=0;
+console.log(a>=b);
+a=("2"===2)?"ok":"not";
+console.log(a);
+
+
