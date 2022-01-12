66ce42de5c5606c542f2d59646ff99a2f2a5dcd7
Vitaliy <69913619+MurkinVitaly@users.noreply.github.com>
2022-01-12 07:52:15 +0200

    Correct all homeworks

---
+++ Js-01-cards-gallery/app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll(".slide");
+
+for (const slide of slides) {
+  slide.addEventListener("click", () => {
+    clearActiveclasses();
+    slide.classList.add("active");
+  });
+}
+
+function clearActiveclasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+}
